12de4b260fc909d6f1f26499a3e1dae3c4d32a1a
Xlebusheck1 <coco.qwe.coco@gmail.com>
2025-03-18 13:20:53 +0300

    корабль
    движение, поворот, качение корабля

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 public class Ship : MonoBehaviour
 {
     Wheel wheel;
+    Collider collider;
+    CharacterController controller;
+    float _maxAngle = 24f;
+    float _shipSpeed = 2f;
+    float _maxIncline = 6f;
+    float _incline = 1f;
+    bool _rightIncline = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         wheel = FindAnyObjectByType<Wheel>();
+        collider = GetComponent<Collider>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void MoveShip()
+    {
+        Vector3 move = new Vector3(0f, 0f, _shipSpeed);
+        controller.Move(move);
+    }
+
+    private void RotateShip()
+    {
+        float angle = wheel.GetCurrentAngle();
+        float shipAngle = Mathf.Clamp(angle / (1080 / _maxAngle), -_maxAngle, _maxAngle);
+
+        float deltaIncline = (_shipSpeed / Mathf.Abs(_incline + 0.00001f)) * Time.deltaTime;
+        Mathf.Clamp(deltaIncline, 0.05f, 0.1f);
+        _incline += deltaIncline * ((_rightIncline) ? 1f : -1f);
+        if (_incline >= _maxIncline) _rightIncline = false;
+        else if (_incline <= -_maxIncline) _rightIncline = true;        
+
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, shipAngle, _incline);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float angle = wheel.GetCurrentAngle();
+        //MoveShip();
+
+        RotateShip();
     }
 }
--- Assets/Scripts/Wheel.cs
+++ Assets/Scripts/Wheel.cs
@@ -4,7 +4,7 @@ public class Wheel : MonoBehaviour
 {
     private bool _canRotate = false;
     float _currentAngle = 0f;
-    float _angularSpeed = 15f;
+    float _angularSpeed = 90f;
     InputSystem_Actions inputActions;
 
     private void Start()
@@ -17,8 +17,10 @@ public class Wheel : MonoBehaviour
         float input = inputActions.Captain.Manage.ReadValue<float>();
         
         if (input > 0) _currentAngle += _angularSpeed * Time.deltaTime;
-        else if (input < 0) _currentAngle -= _angularSpeed * Time.deltaTime;        
-        transform.localEulerAngles = new Vector3(0f, _currentAngle, 0f);
+        else if (input < 0) _currentAngle -= _angularSpeed * Time.deltaTime;  
+        _currentAngle = Mathf.Clamp(_currentAngle, -1080f, 1080f);
+
+        transform.localEulerAngles = new Vector3(-_currentAngle, -90f, 90f);
     }
     public float GetCurrentAngle() { return _currentAngle; }
 }
