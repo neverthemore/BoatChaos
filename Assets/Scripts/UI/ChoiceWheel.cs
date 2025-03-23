b2da31e844dca182dc49c51a3e859784acf6691e
Xlebusheck1 <coco.qwe.coco@gmail.com>
2025-03-23 03:52:46 +0300

    UI CircleMenu

--- Assets/Scripts/UI/ChoiceWheel.cs
+++ Assets/Scripts/UI/ChoiceWheel.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class ChoiceWheel : MonoBehaviour
 {
-    //Открытие UI с колесом, затем в зависимости от положения мышки выбор персонажа
+    //Открытие UI с колесом, затем в зависимости от положения мышки выбор персонажа   
+    [SerializeField] Sprite[] backGroundImages;
+    [SerializeField] Image[] selectionCrosses;
+    private Image selectionWheel;
     private int _currentSelection;
     InputSystem_Actions inputActions;    
     CanvasGroup wheelCanvasGroup;
@@ -33,6 +37,7 @@ public class ChoiceWheel : MonoBehaviour
         if (_currentSelection >= 0 && _currentSelection <= 3)
         {            
             manager.SwitchCharacter(_currentSelection);
+            selectionWheel.sprite = backGroundImages[_currentSelection];
         }
         wheelCanvasGroup.alpha = 0f;        
         Cursor.visible = false;
@@ -41,10 +46,11 @@ public class ChoiceWheel : MonoBehaviour
 
     private void Start()
     {
+        selectionWheel = GetComponentInChildren<Image>();
         inputActions = new InputSystem_Actions();
         manager = CharacterManager.Instance;
         inputActions.Enable();
-        GameObject wheel = GameObject.Find("Wheel");
+        GameObject wheel = GameObject.Find("ChoiseWheel");
         wheelCanvasGroup = wheel.GetComponent<CanvasGroup>();        
     }
 
@@ -52,18 +58,84 @@ public class ChoiceWheel : MonoBehaviour
     {
         _selected = inputActions.Captain.CircleMenu.IsPressed();
         if (_selected) Open();
-        else Close();
+        else Close();        
     }
 
-    public void setSelectionUp() { if (_selected) _currentSelection = 2; }
-    public void setSelectionDown() { if (_selected) _currentSelection = 0; }
-    public void setSelectionRight() { if (_selected) _currentSelection = 3; }
-    public void setSelectionLeft() { if (_selected) _currentSelection = 1; }
+    private void SetSelection(int selection)
+    {
+        if (_selected)
+        {
+            _currentSelection = selection;
+            for (int i = 0; i < selectionCrosses.Length; i++)
+            {
+                if (i == selection)
+                {
+                    Color currentColor = selectionCrosses[selection].color;
+                    currentColor.a = 255f;
+                    selectionCrosses[selection].color = currentColor;
+                }
+                else
+                {
+                    Color currentColor = selectionCrosses[i].color;
+                    currentColor.a = 0f;
+                    selectionCrosses[i].color = currentColor;
+                }
+            }  
 
-    public void SwitchCharacterToCaptain() { if (_selected) manager.SwitchCharacter(0); }
-    public void SwitchCharacterToFranky() { if (_selected) manager.SwitchCharacter(1); }
-    public void SwitchCharacterToUsopp() { if (_selected) manager.SwitchCharacter(2); }
-    public void SwitchCharacterToChopper() { if (_selected) manager.SwitchCharacter(3); }
+        }        
+    }
+
+    public void setSelectionUp()
+    {
+        SetSelection(2);
+    }
+    public void setSelectionDown()
+    {
+        SetSelection(0);
+    }
+    public void setSelectionRight()
+    {
+        SetSelection(3);
+    }
+    public void setSelectionLeft() 
+    {
+        SetSelection(1);
+    }
+
+
+
+    public void SwitchCharacterToCaptain() 
+    {
+        if (_selected)
+        {
+            manager.SwitchCharacter(0);
+            selectionWheel.sprite = backGroundImages[0];
+        }
+    }
+    public void SwitchCharacterToFranky()
+    {
+        if (_selected)
+        {
+            manager.SwitchCharacter(1);
+            selectionWheel.sprite = backGroundImages[1];
+        }
+    }
+    public void SwitchCharacterToUsopp() 
+    {
+        if (_selected)
+        {
+            manager.SwitchCharacter(2);
+            selectionWheel.sprite = backGroundImages[2];
+        }        
+    }
+    public void SwitchCharacterToChopper() 
+    {
+        if (_selected)
+        {
+            manager.SwitchCharacter(3);
+            selectionWheel.sprite = backGroundImages[3];
+        }
+    }
     public int GetCurrentSelection() //Мб через ивент сделать, хз
     {
         return _currentSelection;
