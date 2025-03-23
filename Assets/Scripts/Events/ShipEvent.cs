60999fa7011bd9e8d5d1f776810bc90c573d382d
NoPosh <murin8@mail.ru>
2025-03-23 23:21:33 +0300

    Smth
    Что-то сделал не помню

--- Assets/Scripts/Events/ShipEvent.cs
+++ Assets/Scripts/Events/ShipEvent.cs
@@ -9,11 +9,7 @@ public abstract class ShipEvent : MonoBehaviour
 
     public virtual void StartEvent()
     {
-        if (_eventData.name == "Сломаный штурвал")
-        {
-            Wheel wheel = GetComponentInChildren<Wheel>();
-            wheel.SetRotation(false);
-        }
+        
         //Логика начала ивента (отключение штурвала, управление, понижение скорости...)
         //Вообще возможно нужно делать отдельные менеджеры для ивентов (например менеджер для пробоин, мачты и тд)
         _isActive = true;
@@ -23,11 +19,6 @@ public abstract class ShipEvent : MonoBehaviour
 
     public virtual void FinishEvent()
     {
-        if (_eventData.name == "Сломаный штурвал")
-        {
-            Wheel wheel = GetComponentInChildren<Wheel>();
-            wheel.SetRotation(true);
-        }
         //Соответственно завершение (+ наверное надо сделать проверку)
     }
 }
